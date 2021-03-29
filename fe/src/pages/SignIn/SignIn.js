e973e1b5d57c4bec267863fc3b8b31c5357d0c2c
dino <dinorows@gmail.com>
2021-03-28 21:27:50 -0400

    ${process.env.REACT_APP_API_SERVICE_URL} endpoint

--- fe/src/pages/SignIn/SignIn.js
+++ fe/src/pages/SignIn/SignIn.js
@@ -125,7 +125,8 @@ const SignIn = () => {
 
     // verify user/pwd, get encoded userid as access and refresh tokens in return
     //fetch("http://localhost:5000/login", config)
-    fetch(`${process.env.BE_NETWORK}:${process.env.BE_PORT}/login`, config)
+    //fetch(`${process.env.BE_NETWORK}:${process.env.BE_PORT}/login`, config)
+    fetch(`${process.env.REACT_APP_API_SERVICE_URL}/login`, config)
       .then(response => response.json())
       .then(data => {
 
