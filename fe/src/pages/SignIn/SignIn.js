605c951ee9a0da640ded95af8515b6e2fa42c61a
dino <dinorows@gmail.com>
2021-04-02 03:45:40 -0400

    removed ALB URL prefix from fetches

--- fe/src/pages/SignIn/SignIn.js
+++ fe/src/pages/SignIn/SignIn.js
@@ -127,7 +127,7 @@ const SignIn = () => {
     // verify user/pwd, get encoded userid as access and refresh tokens in return
     //fetch("http://localhost:5000/login", config)
     //fetch(`${process.env.REACT_APP_BE_NETWORK}:${process.env.REACT_APP_BE_PORT}/login`, config)
-    fetch(`${process.env.REACT_APP_API_SERVICE_URL}/login`, config)
+    fetch(`login`, config)
       .then(response => response.json())
       .then(data => {
 
@@ -171,7 +171,7 @@ const SignIn = () => {
     // verify user/pwd, get encoded userid as access and refresh tokens in return
     //fetch("http://localhost:5000/fastlogin", config)
     //fetch(`${process.env.REACT_APP_BE_NETWORK}:${process.env.REACT_APP_BE_PORT}/fastlogin`, config)
-    fetch(`${process.env.REACT_APP_API_SERVICE_URL}/fastlogin`, config)
+    fetch(`fastlogin`, config)
       .then(response => response.json())
       .then(data => {
 
